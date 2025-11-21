01f8f38c062bfe7b45cce178a983555733825e9b
Suhail Kv <suhailkv99@gmail.com>
2025-11-21 15:33:43 +0530

    refactor: comment out unused fields in SectionScoreCache model

--- models/section_score_cache.js
+++ models/section_score_cache.js
@@ -49,11 +49,11 @@ module.exports = (sequelize) => {
     last_calculated_at: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
     graph_type: { type: DataTypes.STRING(100), allowNull: true },
 
-    recommendedNextSteps_immediate: { type: DataTypes.TEXT, allowNull: true, defaultValue: '' },
-    recommendedNextSteps_shortTerm: { type: DataTypes.TEXT, allowNull: true, defaultValue: '' },
-    recommendedNextSteps_longTerm: { type: DataTypes.TEXT, allowNull: true, defaultValue: '' },
-    frontPage_Image: { type: DataTypes.STRING(255), allowNull: true },
-    backPage_Image: { type: DataTypes.STRING(255), allowNull: true },
+    // recommendedNextSteps_immediate: { type: DataTypes.TEXT, allowNull: true, defaultValue: '' },
+    // recommendedNextSteps_shortTerm: { type: DataTypes.TEXT, allowNull: true, defaultValue: '' },
+    // recommendedNextSteps_longTerm: { type: DataTypes.TEXT, allowNull: true, defaultValue: '' },
+    // frontPage_Image: { type: DataTypes.STRING(255), allowNull: true },
+    // backPage_Image: { type: DataTypes.STRING(255), allowNull: true },
 
   }, {
     tableName: 'hr_analyzer_section_scores_cache',
